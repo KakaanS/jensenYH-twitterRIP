f4eda071a15c9eb020edf73fdb58cc8b1700df34
Oscar Wendt <oscar.wendt91@gmail.com>
2023-04-19 14:43:50 +0200

    ändrat i app, borde finnas i prod för context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
+import UserContextProvider from "./context/UserContextProvider";
 
+// Pages import
 import Start from "./pages/PagesStart";
 import Home from "./pages/pagesHome";
 import Profile from "./pages/pagesProfile";
@@ -7,11 +9,13 @@ import Profile from "./pages/pagesProfile";
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Start />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/profile/:id" element={<Profile />}></Route>
-      </Routes>
+      <UserContextProvider>
+        <Routes>
+          <Route path="/" element={<Start />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/profile/:id" element={<Profile />}></Route>
+        </Routes>
+      </UserContextProvider>
     </>
   );
 }

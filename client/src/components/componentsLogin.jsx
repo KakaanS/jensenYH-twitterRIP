3645869452e27652aa9615cd7b41f0350efd8881
GoranMisic <goranmisic@yahoo.com>
2023-05-03 01:58:28 +0200

    Styling for Login/signup/forgot password done

--- client/src/components/componentsLogin.jsx
+++ client/src/components/componentsLogin.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { login } from "../functions/functionsLogin.js";
 import { useNavigate } from "react-router-dom";
+import '../css/Login-signup.css';
 
-function Login() {
+function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
@@ -20,26 +21,35 @@ function Login() {
   };
 
   return (
-    <>
+    <div className="auth-form-container">
+      <h2>Login</h2>
+    <form className="login-form">
+    <label htmlFor="email">email</label>
       <input
         type="text"
         placeholder="Email"
+        id="email"
         value={email}
         onChange={(event) => {
-          setEmail(event.target.value);
+          setEmail(event.target.value);//
         }}
       />
-      n
+      <label htmlFor="password">password</label>
       <input
         type="password"
         placeholder="Password"
+        id="password"
+
         value={password}
         onChange={(event) => {
           setPassword(event.target.value);
         }}
       />
       <button onClick={handleLogin}>LOGIN</button>
-    </>
+      </form>
+      <button className="link-btn" onClick={() => props.onFormSwitch('signup')}> Don't have an account? Register here.</button>
+      <button className="link-btn" onClick={() => props.onFormSwitch('forgotPassword')}> Forgot password? Change it here.</button>
+    </div>
   );
 }
 
--- client/src/components/sidebarOptions.jsx
+++ client/src/components/sidebarOptions.jsx
@@ -3,7 +3,7 @@ import '../css/sidebarOption.css'
 
 function SidebarOptions({active, text ,Icon}) {
   return (
-    <div className={`sidebarOption ${active && 'sidebarOptions--active'}`}>
+    <div className={`sidebarOption ${active && "sidebarOptions--active"}`}>
     <Icon />
       <h2>{text}</h2>
     </div>

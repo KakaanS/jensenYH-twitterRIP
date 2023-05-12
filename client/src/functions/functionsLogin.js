4295d26633f98f0c994c89d5bcaba75fd7b96508
Oscar Wendt <oscar.wendt91@gmail.com>
2023-05-12 15:39:07 +0200

    trying a pure URL in fetch

--- client/src/functions/functionsLogin.js
+++ client/src/functions/functionsLogin.js
@@ -2,7 +2,7 @@ const theConnector = import.meta.env.VITE_API_URL;
 // `${theConnector}`
 
 async function login(email, password) {
-  return await fetch(`${theConnector}/user/login`, {
+  return await fetch(`https://shitter-backend-test.vercel.app/user/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
